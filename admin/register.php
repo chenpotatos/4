723c01ca06a16f8be6412cacae17568d3c400e33
chenpotatos <103394020+chenpotatos@users.noreply.github.com>
2023-07-02 16:26:01 +0800

    Update register.php

--- admin/register.php
+++ admin/register.php
@@ -4,39 +4,46 @@ include 'common.php';
 if ($user->hasLogin() || !$options->allowRegister) {
     $response->redirect($options->siteUrl);
 }
-$rememberName = htmlspecialchars(\Typecho\Cookie::get('__typecho_remember_name'));
-$rememberMail = htmlspecialchars(\Typecho\Cookie::get('__typecho_remember_mail'));
-\Typecho\Cookie::delete('__typecho_remember_name');
-\Typecho\Cookie::delete('__typecho_remember_mail');
+$rememberName = htmlspecialchars(Typecho_Cookie::get('__typecho_remember_name'));
+$rememberMail = htmlspecialchars(Typecho_Cookie::get('__typecho_remember_mail'));
+Typecho_Cookie::delete('__typecho_remember_name');
+Typecho_Cookie::delete('__typecho_remember_mail');
 
 $bodyClass = 'body-100';
 
 include 'header.php';
 ?>
-<div class="typecho-login-wrap">
-    <div class="typecho-login">
-        <h1><a href="https://typecho.org" class="i-logo">Typecho</a></h1>
-        <form action="<?php $options->registerAction(); ?>" method="post" name="register" role="form">
-            <p>
-                <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
-                <input type="text" id="name" name="name" placeholder="<?php _e('用户名'); ?>" value="<?php echo $rememberName; ?>" class="text-l w-100" autofocus />
-            </p>
-            <p>
-                <label for="mail" class="sr-only"><?php _e('Email'); ?></label>
-                <input type="email" id="mail" name="mail" placeholder="<?php _e('Email'); ?>" value="<?php echo $rememberMail; ?>" class="text-l w-100" />
-            </p>
-            <p class="submit">
-                <button type="submit" class="btn btn-l w-100 primary"><?php _e('注册'); ?></button>
-            </p>
-        </form>
-        
-        <p class="more-link">
+<html lang="zh">
+<head>
+  <link href="/admin/css/registerstyle.css" rel="stylesheet" type="text/css" media="all" />
+</head>
+<body>
+
+    <div class="main-w3layouts wrapper">
+        <div class="main-agileinfo">
+            <div class="agileits-top">
+                        <form action="<?php $options->registerAction(); ?>" method="post" 
+name="register" role="form">
+                  <label for="name" class="sr-only"><?php _e('用户名'); ?></label>
+                  <input type="text" id="name" name="name" value="<?php echo $rememberName; ?>" 
+placeholder="<?php _e('用户名'); ?>" class="text-l w-100" autofocus />
+                  <label for="mail" class="sr-only"><?php _e('Email'); ?></label>
+                  <input type="pw" id="mail" name="mail" class="text-l w-100" placeholder="<?php 
+_e('Email'); ?>" />
+                    <input type="submit" value="<?php _e('注册'); ?>">
+                                                  <ul>
+                                                          <p class="more-link">
             <a href="<?php $options->siteUrl(); ?>"><?php _e('返回首页'); ?></a>
             &bull;
             <a href="<?php $options->adminUrl('login.php'); ?>"><?php _e('用户登录'); ?></a>
         </p>
-    </div>
-</div>
+                                                  </ul>
+                </form>
+            </div>
+       </div>
+
+</body>
+</html>
 <?php 
 include 'common-js.php';
 ?>
